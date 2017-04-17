ef99d0e0bed585d3f5adbddcea91d38f8ec8e147
Arpit Goyal <arpit.go4@gmail.com>
2017-04-17 15:56:03 +0530

    random color changing added with new quote

--- src/components/App.component.js
+++ src/components/App.component.js
@@ -11,14 +11,19 @@ export default class App extends React.Component {
 			quote: 'This a quote!!',
 			author: 'Arpit Goyal'
 		};
+		this.randomColors = [
+			'pink', 'red', 'black', 'green', 'blue', 'grey'
+		];
 	}
 
 	render(){
+		const length = this.randomColors.length;
+		const color = this.randomColors[Math.floor(Math.random() * length)];
 		return (
-			<div style={{ backgroundColor: 'red'}} className="container-fluid">
+			<div style={{ backgroundColor: `${color}` }} className="container-fluid">
 				<div className="row">
 					<div style={{ marginTop: '12.5%', marginLeft: '30%'}} className="col-xs-5 col-xs-offset-3">
-						<QuoteContainer color="red" 
+						<QuoteContainer color={color} 
 										backgroundColor="white"
 										quote={this.state.quote}
 										author={this.state.author}

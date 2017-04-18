a7f0f0223b3e7ccb37a143f635c26d4a6437898e
Arpit Goyal <arpit.go4@gmail.com>
2017-04-18 16:33:05 +0530

    react-transition group lib added

--- src/components/Quote.component.js
+++ src/components/Quote.component.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 export default class Quote extends React.Component {
 	render() {
@@ -9,7 +10,7 @@ export default class Quote extends React.Component {
 				<div style={{ marginTop: '5%' }} className="col-xs-10 col-xs-offset-1 text-center">
 					<i style={{ fontSize: `${quoteIconFontSize}em`, position: 'relative', bottom: '7px' }} className="fa fa-quote-left"></i>
 					&nbsp;&nbsp;
-					<p  style={{ display: 'inline', fontSize: `${quoteFontSize}em` }}  className="">{this.props.quote}</p>
+					<p style={{ display: 'inline', fontSize: `${quoteFontSize}em` }}  className="">{this.props.quote}</p>
 				</div>
 				<div style={{ marginTop: '3%', paddingRight: '5.5%' }} className="col-xs-11 text-right ">
 					<footer style={{ fontSize: '1.2em', fontWeight: '100' }} className="blockquote">- {this.props.author}</footer>

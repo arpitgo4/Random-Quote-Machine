613fdbd971b1f74a271d53cecfe9d52e1c956bc6
Arpit Goyal <arpit.go4@gmail.com>
2017-04-19 13:07:17 +0530

    react-css-transition added, but not looking good

--- src/components/Quote.component.js
+++ src/components/Quote.component.js
@@ -16,16 +16,23 @@ export default class Quote extends React.Component {
 			<div id="quote" className="row" style={{ color: this.props.color }}>
 				
 					<div style={{ marginTop: '5%' }} className="col-xs-10 col-xs-offset-1 text-center">
-						<i style={{ fontSize: `${quoteIconFontSize}em`, position: 'relative', bottom: '7px' }} className="fa fa-quote-left to-fade"></i>
+						<i style={{ fontSize: `${quoteIconFontSize}em`, position: 'relative', bottom: '7px' }} className="fa fa-quote-left"></i>
 						&nbsp;&nbsp;
-						
-						<p style={{ display: 'inline', fontSize: `${quoteFontSize}em` }}  className="to-fade">{this.props.quote}</p>
+						<ReactCSSTransitionGroup
+							transitionName="quote"
+							transitionEnterTimeout={4000}
+							transitionLeaveTimeout={1}>
+
+							{(
+								() => <p key={Math.random()} style={{ display: 'inline', fontSize: `${quoteFontSize}em` }}  
+											className="">{this.props.quote}</p>
+							)()}
 							
-						
+						</ReactCSSTransitionGroup>
 						
 					</div>
 					<div style={{ marginTop: '3%', paddingRight: '5.5%' }} className="col-xs-11 text-right ">
-						<footer style={{ fontSize: '1.2em', fontWeight: '100' }} className="blockquote to-fade">- {this.props.author}</footer>
+						<footer style={{ fontSize: '1.2em', fontWeight: '100' }} className="blockquote">- {this.props.author}</footer>
 					</div>
 				
 			</div>
@@ -43,4 +50,4 @@ export default class Quote extends React.Component {
 					transitionEnterTimeout={500}
 					transitionLeaveTimeout={500}>
 
-					</ReactCSSTransitionGroup>*/
+					</ReactCSSTransitionGroup>*/0
